8f96fa68be27d5fb0cd6c05f0c448cd645e8e2c8
Marco.Silipo <m.silipo@web.de>
2016-10-08 17:40:05 +0200

    Files

---
+++ CommandLineParameterParser/Command.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+
+namespace CommandLineParameterParser
+{
+    public struct Command
+    {
+        /// <summary>
+        /// Enum representing the different command types that are available;
+        /// </summary>
+        public enum EKind
+        {
+            /// <summary>
+            /// <para>Simple flag command. Only one occurance is allowed, no arguments passed.</para>
+            /// </summary>
+            Flag,
+            /// <summary>
+            /// <para>Valid path to file. Path will be passed as argument.</para>
+            /// </summary>
+            Path,
+            /// <summary>
+            /// Property with a value bound to it. Argument value will be passed to this.
+            /// </summary>
+            Property
+        }
+
+        public EKind Kind;
+        public string Name;
+        public string DefaultValue;
+        public Action<TextWriter, string> Action;
+
+        private Command(EKind Kind, string Name, Action<TextWriter, string> Action)
+        {
+            this.Kind = Kind;
+            this.Name = Name;
+            this.Action = Action;
+            this.DefaultValue = string.Empty;
+        }
+        private Command(EKind Kind, Action<TextWriter, string> Action)
+        {
+            this.Kind = Kind;
+            this.Name = string.Empty;
+            this.Action = Action;
+            this.DefaultValue = string.Empty;
+        }
+        private Command(EKind Kind, string Name, Action<TextWriter, string> Action, string DefaultValue)
+        {
+            this.Kind = Kind;
+            this.Name = Name;
+            this.Action = Action;
+            this.DefaultValue = DefaultValue;
+        }
+
+        /// <summary>
+        /// Creates a new Flag command.
+        /// </summary>
+        /// <param name="Name">Name of this flag</param>
+        /// <param name="Action">Action for this Command. TextWriter is for any output. string will be empty.</param>
+        /// <returns>New Command with of the kind Flag.</returns>
+        public static Command CreateFlag(string Name, Action<TextWriter, string> Action)
+        {
+            return new Command(EKind.Flag, Name, Action);
+        }
+        /// <summary>
+        /// Creates a new Path command.
+        /// It is recommended to only have one of theese.
+        /// </summary>
+        /// <param name="Action">Action for this Command. TextWriter is for any output. string will contain the path provided.</param>
+        /// <returns>New Command with of the kind Path.</returns>
+        public static Command CreatePath(Action<TextWriter, string> Action)
+        {
+            return new Command(EKind.Path, Action);
+        }
+        /// <summary>
+        /// Creates a new Property command. Property will always trigger if DefaultValue got assigned.
+        /// </summary>
+        /// <param name="Name">Name of this Property</param>
+        /// <param name="Action">Action for this Command. TextWriter is for any output. string will be the assigned value or the default value.</param>
+        /// <param name="DefaultValue">Default value for this command in case it was never added to the arrg list. Empty value means this wont trigger then.</param>
+        /// <returns>New Command with of the kind Flag.</returns>
+        public static Command CreateProperty(string Name, Action<TextWriter, string> Action, string DefaultValue = "")
+        {
+            return new Command(EKind.Path, Name, Action, DefaultValue);
+        }
+    }
+}
---
+++ CommandLineParameterParser/Parser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace CommandLineParameterParser
+{
+    public class Parser
+    {
+        private IEnumerable<Command> CmdList;
+        private TextWriter Out;
+        public Parser(IEnumerable<Command> CmdList)
+        {
+            this.CmdList = CmdList;
+            this.Out = Console.Out;
+        }
+        public Parser(IEnumerable<Command> CmdList, TextWriter StdOut)
+        {
+            this.CmdList = CmdList;
+            this.Out = StdOut;
+        }
+
+
+        /// <summary>
+        /// Displays help text on output stream
+        /// </summary>
+        public void DisplayHelp()
+        {
+
+        }
+
+        /// <summary>
+        /// Checks provided arguments for commands and executes them.
+        /// </summary>
+        /// <param name="args">string array passed to the tool</param>
+        public void Check(string[] args)
+        {
+            var enumerator = args.GetEnumerator();
+            List<Command> PropertyCommands = new List<Command>();
+            foreach(var it in this.CmdList)
+            {
+                if(it.Kind == Command.EKind.Property && !string.IsNullOrWhiteSpace(it.DefaultValue))
+                {
+                    PropertyCommands.Add(it);
+                }
+            }
+            while (enumerator.MoveNext())
+            {
+                string arg = enumerator.Current as string;
+                if (string.IsNullOrWhiteSpace(arg))
+                    continue;
+                char firstChar = arg[0];
+                foreach (var cmd in this.CmdList)
+                {
+                    switch (cmd.Kind)
+                    {
+                        case Command.EKind.Flag:
+                            {
+                                if (firstChar != '-' && firstChar != '/')
+                                    continue;
+                                if(arg.Substring(1).Equals(cmd.Name))
+                                {
+                                    cmd.Action(this.Out, string.Empty);
+                                }
+                            }
+                            break;
+                        case Command.EKind.Path:
+                            {
+                                if (firstChar == '-' || firstChar == '/')
+                                    continue;
+                                cmd.Action(this.Out, arg);
+                            }
+                            break;
+                        case Command.EKind.Property:
+                            {
+                                if (firstChar != '-' && firstChar != '/')
+                                    continue;
+                                if (arg.Substring(1).Equals(cmd.Name))
+                                {
+                                    enumerator.MoveNext();
+                                    if(PropertyCommands.Contains(cmd))
+                                    {
+                                        PropertyCommands.Remove(cmd);
+                                    }
+                                    cmd.Action(this.Out, enumerator.Current as string);
+                                }
+                            }
+                            break;
+                        default: throw new NotImplementedException();
+                    }
+                }
+                foreach(var cmd in PropertyCommands)
+                {
+                    cmd.Action(this.Out, cmd.DefaultValue);
+                }
+            }
+        }
+    }
+}
